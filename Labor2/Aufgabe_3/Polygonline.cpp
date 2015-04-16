f21bb11e19e1ae3b2a9eb9c1db7dae6295ca5fec
BattleCake451 <johannes.steinhuelb@web.de>
2015-04-16 21:56:29 +0200

    Lab2 Aufgabe 3
    Hab mir voll die Mühe gegeben :-)

--- Labor2/Aufgabe_3/Polygonline.cpp
+++ Labor2/Aufgabe_3/Polygonline.cpp
@@ -1,19 +1,129 @@
 #include "Polygonline.h"
+#include <iostream>
 
-PlgElement::~PlgElement()
+using namespace std;
+
+//--------------------------------------------------------------------------------------
+//Standart Konstruktor.
+//--------------------------------------------------------------------------------------
+Polygonline::Polygonline() : m_StartElement(nullptr), m_LastElement(nullptr)
 {
-	if (m_Next)
+}
+
+//--------------------------------------------------------------------------------------
+//Konstruktor.
+//--------------------------------------------------------------------------------------
+Polygonline::Polygonline(Point point)
+{
+	m_StartElement = new PlgElement(point);
+	m_LastElement = m_StartElement;
+}
+
+//--------------------------------------------------------------------------------------
+//Dekonstruktor.
+//--------------------------------------------------------------------------------------
+Polygonline::~Polygonline()
+{
+	if (m_StartElement)
 	{
-		delete m_Next;
-		m_Next = nullptr;
+		delete m_StartElement;
+		m_StartElement = nullptr;
+	}
+
+	if (m_LastElement)
+	{
+		m_LastElement = nullptr;
 	}
 }
 
-Polygonline::Polygonline()
+//--------------------------------------------------------------------------------------
+//Fügt einen Punkt zum Polygonzug hinzu.
+//--------------------------------------------------------------------------------------
+Polygonline& Polygonline::addPoint(Point point)
 {
+	if (m_StartElement == nullptr)
+	{
+		m_StartElement = new PlgElement(point);
+		m_LastElement = m_StartElement;
+
+	}
+	else
+	{
+		m_LastElement->setNext(new PlgElement(point));
+		m_LastElement = m_LastElement->getNext();
+	}
+
+	return(*this);
 }
 
-
-Polygonline::~Polygonline()
+//--------------------------------------------------------------------------------------
+//Fügt einen anderen Polygonzug diesem hinzu. Es wird eine Tiefe kopie des alten Polygonzugs angelegt.
+//--------------------------------------------------------------------------------------
+void Polygonline::appendPolygonline(Polygonline& polygonline)
 {
+	PlgElement *iterator = polygonline.getStartElement();
+
+	while (iterator)
+	{
+		//Tiefe Kopie anlegen.
+		addPoint(iterator->getPoint());
+		iterator = iterator->getNext();
+	}
+
+}
+
+//--------------------------------------------------------------------------------------
+//Gibt das erste Element der Liste zurück.
+//--------------------------------------------------------------------------------------
+PlgElement * Polygonline::getStartElement()
+{
+	return(m_StartElement);
+}
+
+//--------------------------------------------------------------------------------------
+//Gibt das letzte Element der Liste zurück.
+//--------------------------------------------------------------------------------------
+PlgElement* Polygonline::getLastElement()
+{
+	return(m_LastElement);
+}
+
+//--------------------------------------------------------------------------------------
+//Bewegt alle Punkte des Polygonzugs.
+//--------------------------------------------------------------------------------------
+void Polygonline::move(const double dx, const double dy)
+{
+	PlgElement *iterator = m_StartElement;
+
+	while (iterator)
+	{
+		iterator->getPoint().move(dx, dy);
+		iterator = iterator->getNext();
+	}
+}
+
+//--------------------------------------------------------------------------------------
+//Gibt alle Punkte des Polygonzugs aus.
+//--------------------------------------------------------------------------------------
+void Polygonline::print()
+{
+	PlgElement *iterator = m_StartElement;
+	cout << "|";
+
+	while (iterator)
+	{
+		iterator->getPoint().print(false);
+
+		//Nur einen Bindestrich schreiben wenn es auch ein nächstes Element gibt.
+		if (iterator->getNext() != nullptr)
+		{
+			cout << " - ";
+		}
+
+		iterator = iterator->getNext();
+
+		
+	}
+
+	cout << "|" << endl;
 }
--- Labor2/Aufgabe_3/Polygonline.h
+++ Labor2/Aufgabe_3/Polygonline.h
@@ -1,20 +1,78 @@
 #pragma once
 #include "Point.h"
 
+//Listen Element für eine einfach verkettete Liste.
 class PlgElement
 {
 	public:
+		//Standart Konstruktor.
+		PlgElement();
+
+		//Konstruktor.
+		PlgElement(Point point);
+
+		//Dekonstruktor.
 		~PlgElement();
 
+	public:
+		//Gibt das nächste Elemenet der Liste zurück.
+		PlgElement *getNext();
+
+		//Gibt den Point des aktuellen Elements zurück.
+		Point& getPoint();
+
+		//Setzt das nächste Element der Liste.
+		void setNext(PlgElement *next);
+
+		//Setzt den Point des aktuellen Listen Elements.
+		void setPoint(Point point);
+
 	private:
+
+		//Position des Linienpunktes.
 		Point m_Point;
+
+		//Nächstes Element in der Liste.
 		PlgElement *m_Next;
 };
 
+//Klasse für einen Polygonzug.
 class Polygonline
 {
 	public:
+		//Konstruktor.
 		Polygonline();
+
+		//Standart Konstruktor.
+		Polygonline(Point point);
+
+		//Dekonstruktor.
 		~Polygonline();
+
+		//Gibt alle Punkte des Polygonzugs aus.
+		void print();
+
+		//Bewegt alle Punkte des Polygonzugs.
+		void move(const double dx, const double dy);
+
+		//Fügt einen Punkt zum Polygonzug hinzu.
+		Polygonline& addPoint(Point point);
+
+		//Fügt einen anderen Polygonzug diesem hinzu. Es wird eine Tiefe kopie des alten Polygonzugs angelegt.
+		void appendPolygonline(Polygonline& polygonline);
+
+	public:
+		//Gibt das erste Element der Liste zurück.
+		PlgElement *getStartElement();
+
+		//Gibt das letzte Element der Liste zurück.
+		PlgElement *getLastElement();
+
+	private:
+		//Erstes Element der Liste.
+		PlgElement *m_StartElement;
+
+		//Letztes Element der Liste.
+		PlgElement *m_LastElement;
 };
 

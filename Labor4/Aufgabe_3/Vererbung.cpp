0e16c43cd9c6646cc9ec3d4f89bf02430d9c0991
Kaitsh <d4n13l.gl1nk4@gmail.com>
2015-06-12 18:58:17 +0200

    Bei virtuellen Klassen muss der Konstruktor nochmal aufgerufen werden... Bringt aber auch nix

--- Labor4/Aufgabe_3/Vererbung.cpp
+++ Labor4/Aufgabe_3/Vererbung.cpp
@@ -97,7 +97,7 @@ class C3 : public B
 
 class D1 :  public C1, public C2 {
 public:
-	D1(int C1_a = 0xD1C1A, int C2_a = 0xD1C2A,int C1_b = 0xD1C1B, int C2_b = 0xD1C2B): C1(C1_a, C1_b), C2(C2_a, C2_b) {}
+	D1(int C1_a = 0xD1C1A, int C2_a = 0xD1C2A, int C1_b = 0xD1C1B, int C2_b = 0xD1C2B) :A(), B(), C1(C1_a, C1_b), C2(C2_a, C2_b) {}
 	
 	void print() 
 	{
@@ -113,9 +113,8 @@ public:
 class D2 :public C1, public C2, public C3 
 {
 	public:
-		D2(int C1_a = 0xD2C1A, int C2_a = 0xD2C2A, int C3_a = 0xD2C3A, 
-		   int C1_b = 0xD2C1B, int C2_b = 0xD2C2B, int C3_b = 0xD2C3B) 
-		   :C1(C1_a, C1_b), C2(C2_a, C2_b), C3(C3_a, C3_b) {}
+		D2(int C1_a = 0xD2C1A, int C2_a = 0xD2C2A, int C3_a = 0xD2C3A,
+		   int C1_b = 0xD2C1B, int C2_b = 0xD2C2B, int C3_b = 0xD2C3B) : C1(C1_a, C1_b), C2(C2_a, C2_b), C3(C3_a, C3_b) {}
 
 		void print() 
 		{ 

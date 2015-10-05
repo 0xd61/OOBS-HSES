c399902e26654f412655acf7fe71b1ddc24425c0
Kaitsh <kaitsh@d-git.de>
2015-10-05 10:57:15 +0200

    ADSBSentenceFactory

--- 3 Semester/Lab1/src/senser/ADSBSentenceFactory.java
+++ 3 Semester/Lab1/src/senser/ADSBSentenceFactory.java
@@ -4,24 +4,25 @@ import java.util.regex.Pattern;
 
 public class ADSBSentenceFactory implements ADSBSentenceFactoryInterface
 {
-	//TODO: define a regular express to filter only relevant messages: 
+	//DONE: define a regular express to filter only relevant messages:
 	//{"subscribe":["message","ads.sentence","1379574427.9127481!ADS-B*8D40675258BDF05CDBFB59DA7D6F;\r\n"]}
-	private static final String patAdsbJson = "test";
+	private static final String patAdsbJson = "\\d+\\.\\d+!ADS-B\\*[A-Z0-9]+";
 
 	@Override
 	public ADSBSentence fromWebdisJson(String json)
 	{		
 		if ( Pattern.matches ( patAdsbJson, json ) ) 
 		{					
-			//TODO: Get distinct values from the json string
-			//String timestamp = ...;
-			//String dfca 	 = ...;
-			//String icao 	 = ...;
-			//String payload 	 = ...;
-			//String parity 	 = ...;
+			//DONE: Get distinct values from the json string
+			String timestamp = json.split("\\d+\\.\\d+")[0];
+			String dfca 	 = json.split("\\*\\w{2}")[0].replace("*","");
 
-			//return new ADSBSentence(timestamp, dfca, icao, payload, parity);
-			return null;
+			String jsonNEU = json.split("\\*")[1];
+			String icao 	 = jsonNEU.substring(2,8);
+			String payload 	 = jsonNEU.substring(9,23);
+			String parity 	 = jsonNEU.substring(24,30);
+
+			return new ADSBSentence(timestamp, dfca, icao, payload, parity);
 		}
 		else
 		{

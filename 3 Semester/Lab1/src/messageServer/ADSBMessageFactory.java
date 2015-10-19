76128f6f5627a2045435463a88f9ff6706c9e67d
BattleCake451 <johannes.steinhuelb@web.de>
2015-10-19 10:39:50 +0200

    Lab 2 - Factory

--- 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
+++ 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
@@ -11,6 +11,22 @@ public class ADSBMessageFactory implements ADSBMessageFactoryInterface
     @Override
     public ADSBMessage fromADSBSentence(ADSBSentence adsbSentence)
     {
+        int payloadInInteger = Integer.parseInt(adsbSentence.getPayload());
+        String payloadInBin = Integer.toBinaryString(payloadInInteger);
+
+        String substringMessageType = payloadInBin.substring(0,4);
+        int messageType = Integer.parseInt(substringMessageType);
+
+        switch (messageType)
+        {
+            case 0:
+                //POSITION MESSAGE.
+                break;
+
+            
+        }
+
+
         return null;
     }
 }

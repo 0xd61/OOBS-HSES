c7866b729366382106e62eef10f0bcf579e2c42d
Kaitsh <kaitsh@d-git.de>
2015-11-09 08:55:10 +0100

    Messagetype fertig geparsed

--- 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
+++ 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
@@ -21,30 +21,46 @@ public class ADSBMessageFactory implements ADSBMessageFactoryInterface
         int TypeCode = Integer.parseInt(substringTypeCode);
         int SubtypeCode = Integer.parseInt(substringSubtypeCode);
 
+        //Airborne Position Message
         if(TypeCode == 0 || (TypeCode >= 9 && TypeCode <= 18)|| (TypeCode >= 20 && TypeCode <= 22))
         {
             //Messagetype parsen
             int type = Integer.parseInt(payloadInBin.substring(0, 4));
             int surveillance = Integer.parseInt(payloadInBin.substring(5, 6));
             int nicSupplement = Integer.parseInt(payloadInBin.substring(7, 7));
-            int altitude = Integer.parseInt(payloadInBin.substring(8, 19));
+
+            String strAltitude = payloadInBin.substring(8, 14);
+            String strAltitude2 = payloadInBin.substring(16,19);
+            int altitude = Integer.parseInt(strAltitude + strAltitude2);
+
             int timeFlag = Integer.parseInt(payloadInBin.substring(20, 20));
             int CPRFormat = Integer.parseInt(payloadInBin.substring(21, 21));
             int CPREncodedLat = Integer.parseInt(payloadInBin.substring(22, 38));
             int CPRLongitude = Integer.parseInt(payloadInBin.substring(39, 55));
 
+            //QBit und Altitude Berechnung
+            int qBit = Integer.parseInt(payloadInBin.substring(15, 15));
+            if (qBit == 0)
+                altitude *= 100;
+            else
+                altitude *= 25;
+
+
             ADSBMessage message = new ADSBAirbornePositionMessage(surveillance,nicSupplement,altitude,timeFlag,CPRFormat,CPRLongitude,CPREncodedLat);
             return message;
         }
 
+        //Aircraft Identification Message
         if(TypeCode >= 1 && TypeCode <= 4)
         {
+            //Messagetype parsen
 
         }
 
-        if(TypeCode == 19)
+        //Aircraft Velociry Message
+        if(TypeCode == 19 && (SubtypeCode >= 1 && SubtypeCode <=4))
         {
-            //Aircraft Velocity
+            
         }
 
         return null;

2c02f6d212b55926cbe0c14c6e973cc5faac42db
Kaitsh <kaitsh@d-git.de>
2015-11-09 09:37:24 +0100

    Message done

--- 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
+++ 3 Semester/Lab1/src/messageServer/ADSBMessageFactory.java
@@ -25,7 +25,6 @@ public class ADSBMessageFactory implements ADSBMessageFactoryInterface
         if(TypeCode == 0 || (TypeCode >= 9 && TypeCode <= 18)|| (TypeCode >= 20 && TypeCode <= 22))
         {
             //Messagetype parsen
-            int type = Integer.parseInt(payloadInBin.substring(0, 4));
             int surveillance = Integer.parseInt(payloadInBin.substring(5, 6));
             int nicSupplement = Integer.parseInt(payloadInBin.substring(7, 7));
 
@@ -53,14 +52,40 @@ public class ADSBMessageFactory implements ADSBMessageFactoryInterface
         //Aircraft Identification Message
         if(TypeCode >= 1 && TypeCode <= 4)
         {
-            //Messagetype parsen
+            char[] ascii = {'@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                            '[','\\',']','^','_',' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+                            '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?'};
+            int emitterCategory = Integer.parseInt(payloadInBin.substring(5,7));
+            String aircraftId = payloadInBin.substring(8,55);
 
+            aircraftId = Character.toString(ascii[Integer.parseInt(aircraftId.substring(0,5))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(6,11))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(12,17))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(18,23))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(24,29))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(30,35))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(36,41))]) +
+                         Character.toString(ascii[Integer.parseInt(aircraftId.substring(42,47))]);
+
+            ADSBMessage message = new ADSBAircraftIdentificationMessage(emitterCategory,aircraftId);
         }
 
-        //Aircraft Velociry Message
+        //Aircraft Velocity Message
         if(TypeCode == 19 && (SubtypeCode >= 1 && SubtypeCode <=4))
         {
-            
+            int subtype = Integer.parseInt(payloadInBin.substring(5,7));
+            int intentChange = Integer.parseInt(payloadInBin.substring(8,8));
+            int reservedA = Integer.parseInt(payloadInBin.substring(9, 9));
+            int navigationAccuracy = Integer.parseInt(payloadInBin.substring(10,12));
+
+
+            int speed=0;
+            int heading=0;
+            int verticalRateSource=0;
+            int verticalSpeed=0;
+
+            ADSBMessage message = new ADSBAirborneVelocityMessage(subtype,intentChange,reservedA,navigationAccuracy,speed,heading,verticalRateSource,verticalSpeed);
+            return message;
         }
 
         return null;
